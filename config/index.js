9a9f6faefd3481581404d6338130fc8e8bd12a2c
codyguha <aalo.guha@gmail.com>
2016-05-19 09:57:45 -0700

    ngroked

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ module.exports = {
     kik: {
       apiKey: 'ea2ff779-c0cb-4ee8-9e22-377629a80c5d', // [Non functional example] 0a275f34-31da-4ds1-b3c9-d4576f38bac8
       botUsername: 'surveychicken', // [Non functional example] startkitbot
-      baseUrl: 'http://78efa8ff.ngrok.io' // [Non functional example] http://4b7e3c1e.ngrok.io
+      baseUrl: 'http://a0157243.ngrok.io' // [Non functional example] http://4b7e3c1e.ngrok.io
     }
   }
 }['local']
